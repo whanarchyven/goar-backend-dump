7f65e6994fc949971d2a000f382ab46731492db7
Vladislav <vladislah@gmail.com>
2022-08-24 17:42:18 +0300

    fix

--- fitness/asgi.py
+++ fitness/asgi.py
@@ -8,14 +8,13 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
 
 import os
-
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fitness.settings")
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-
 from chat import routing
 from chat.middleware import QueryAuthMiddleware
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fitness.settings")
+
 django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
