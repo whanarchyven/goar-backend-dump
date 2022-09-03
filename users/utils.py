208d627dc529d7fa83e3a060886b324b6fd697a6
Vladislav <vladislah@gmail.com>
2022-09-03 08:16:37 +0300

    from email name

--- users/utils.py
+++ users/utils.py
@@ -18,7 +18,12 @@ def send_new_user_email(
 
     txt_body = render_to_string(txt_template, context)
 
-    message = EmailMultiAlternatives(subject=subject, body=txt_body, to=[to, ], from_email="info@skill-app.ru")
+    message = EmailMultiAlternatives(
+        subject=subject,
+        body=txt_body,
+        to=[to, ],
+        from_email="Метод Гоар <info@skill-app.ru>"
+    )
 
     if html_template:
         body = render_to_string(html_template, context)
